12aa372497f0d8629ff39dd12c0d9685178b610d
Sam Patterson <sampatterson@Sams-MacBook-Pro.local>
2023-07-18 21:59:09 -0400

    stopping point 07/18/23

--- lib/views/notes/new_note_view.dart
+++ lib/views/notes/new_note_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:somenotes/services/auth/auth_service.dart';
+import 'package:somenotes/services/crud/notes_service.dart';
 
 class NewNoteView extends StatefulWidget {
   const NewNoteView({super.key});
@@ -8,6 +10,56 @@ class NewNoteView extends StatefulWidget {
 }
 
 class _NewNoteViewState extends State<NewNoteView> {
+  DatabaseNote? _note;
+  late final NotesService _notesService;
+  late final TextEditingController _textController;
+
+  @override
+  void initState() {
+    _notesService = NotesService();
+    _textController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _deleteNoteIfTextIsEmpty();
+    _saveNoteIfTextNotEmpty();
+    _textController.dispose();
+    super.dispose();
+  }
+
+  Future<DatabaseNote> createNewNote() async {
+    final existingNote = _note;
+    if (existingNote != null) {
+      return existingNote;
+    }
+    final currentUser = AuthService.firebase().currentUser!;
+    final email = currentUser.email!;
+    final owner = await _notesService.getUser(email: email);
+    return await _notesService.createNote(owner: owner);
+  }
+
+  void _deleteNoteIfTextIsEmpty() {
+    final note = _note;
+    if (_textController.text.isEmpty && note != null) {
+      _notesService.deleteNote(id: note.id);
+    }
+  }
+
+  void _saveNoteIfTextNotEmpty() async {
+    final note = _note;
+    if (_textController.text.isNotEmpty && note != null) {
+      await _notesService.updateNote(note: note, text: _textController.text);
+    }
+  }
+
+  // void _textControllerListener() async {
+  //   _textController.addListener(() {
+
+  //   })
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -75,7 +75,8 @@ class _NotesViewState extends State<NotesView> {
                 builder: (context, snapshot) {
                   switch (snapshot.connectionState) {
                     case ConnectionState.waiting:
-                      return const Text('waiting for all notes');
+                    case ConnectionState.active:
+                      return const Text('Waiting for all notes');
                     default:
                       return const CircularProgressIndicator();
                   }
